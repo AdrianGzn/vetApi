0001e677f52ac20af5e5ff807449bddbc3215736
AdrianGzn <233318@ids.upchiapas.edu.mx>
2026-07-27 02:00:24 -0600

    Adecuacion

--- database.go
+++ database.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+// getEnv devuelve el valor de la variable de entorno o un valor por defecto.
+func getEnv(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
+		return value
+	}
+	return defaultValue
+}
+
 func ConnectDatabase() {
-	dsn := "rubi:1234@tcp(54.158.229.20:3306)/Veterinary?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := getEnv("DB_USER", "rubi")
+	dbPass := getEnv("DB_PASS", "1234")
+	dbHost := getEnv("DB_HOST", "54.158.229.20")
+	dbPort := getEnv("DB_PORT", "3306")
+	dbName := getEnv("DB_NAME", "Veterinary")
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName,
+	)
+
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Error connecting to database")
+		log.Fatalf("Error conectando a la base de datos: %v", err)
 	}
 	DB = database
 }
